99bcf1de0f6df91cbe4eed452e4b40e4ac41e6e3
ognjend05 <ooggii05@gmail.com>
2025-12-23 14:39:20 +0100

    create Accommodation entity

---
+++ src/main/java/com/raf/mrworldwide/domain/entities/plan/Accommodation.java
@@ -0,0 +1,53 @@
+package com.raf.mrworldwide.domain.entities.plan;
+
+import com.raf.mrworldwide.domain.entities.BaseEntityUUID;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "\"accommodations\"")
+public class Accommodation extends BaseEntityUUID {
+
+    @OneToOne
+    @JoinColumn(name = "destination_id")
+    private Destination destination;
+
+    private String hotelId;
+    private String name;
+    private String address;
+    private String imageUrl;
+    private String bookingUrl;
+
+    private Double starRating;
+    private Double reviewScore;
+
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+    private Double priceTotal;
+    private String currencyCode;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Destination that = (Destination) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
