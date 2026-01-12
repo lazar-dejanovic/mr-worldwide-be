f2e3f0860ecfcb10a64557dd7ef1d5a01280366e
lazar-dejanovic <lazzar99@gmail.com>
2026-01-12 21:56:53 +0100

    Changes by ognjendejanovic

--- src/main/java/com/raf/mrworldwide/domain/mappers/UserMapper.java
+++ src/main/java/com/raf/mrworldwide/domain/mappers/UserMapper.java
@@ -3,6 +3,7 @@ package com.raf.mrworldwide.domain.mappers;
 import com.raf.mrworldwide.domain.dto.BaseEntityDto;
 import com.raf.mrworldwide.domain.dto.user.UserDto;
 import com.raf.mrworldwide.domain.entities.user.User;
+import com.raf.mrworldwide.security.UserRegisterRequest;
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
@@ -13,5 +14,20 @@ public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
     UserDto toDto(User user);
+
+    @Mapping(target = "base", source = "user")
+    @Mapping(target = "accessToken", source = "token")
+    UserDto toDto(User user, String token);
+
+    User fromRegisterRequest(UserRegisterRequest request);
+
+    default BaseEntityDto toBase(User user) {
+        if (user == null) return null;
+        return new BaseEntityDto(
+                user.getId(),
+                user.getCreatedOn(),
+                user.getUpdatedOn()
+        );
+    }
 
 }
